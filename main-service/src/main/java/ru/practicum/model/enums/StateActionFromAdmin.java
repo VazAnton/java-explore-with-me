6f0536dc38e9588eb160b25459b4605ee4c47393
VazAnton <vazev.anton@yandex.ru>
2024-06-18 22:46:27 +0700

    feat: RequestService

--- main-service/src/main/java/ru/practicum/model/enums/StateActionFromAdmin.java
+++
@@ -1,6 +0,0 @@
-package ru.practicum.model.enums;
-
-public enum StateActionFromAdmin {
-    PUBLISH_EVENT,
-    REJECT_EVENT
-}
--- main-service/src/main/java/ru/practicum/model/enums/StateActionLikeUser.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.model.enums;
-
-public enum StateActionLikeUser {
-
-    SEND_TO_REVIEW,
-    CANCEL_REVIEW
-}
